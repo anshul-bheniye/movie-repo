91f8354a4b31b18a666446a657f61d1617d79562
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-02 09:48:27 +0530

    initial header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,40 @@
- 
+ import {Link,NavLink} from 'react-router-dom';
+//  import Logo from '../assets/logo.png';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+      <header>
+          <nav>
+            <Link to='/'>
+              {/* <img src={Logo} alt="movies repo" /> */}
+              <span>Movies Repo</span>
+            </Link>
+          </nav>
+
+          <ul>
+            <li>
+              <NavLink to='/'>Home</NavLink>
+            </li>
+            <li>
+            <NavLink to='/popular'>Popular</NavLink>
+            </li>
+            <li>
+            <NavLink to='/topRated'>Top Rated</NavLink>
+            </li>
+            <li>
+            <NavLink to='/upcoming'>Upcoming</NavLink>
+            </li>
+          </ul>
+
+          <div>
+            <button>Mode</button>
+            <input 
+              type="text" 
+              name="input" 
+              placeholder='search movies' 
+              autoComplete='off' 
+            />
+          </div>
+      </header>
   )
 }
