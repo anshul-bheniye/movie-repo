49ce6f8ef8f0cb08e9e0333263420f1fd97f61fb
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-02 10:53:08 +0530

    tailwind css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {App} from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.css';
  
 
 const rootElement = document.getElementById('root')
