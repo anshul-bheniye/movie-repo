bc522b4548036494a39aef4c10cc183997768301
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-02 09:49:15 +0530

    import component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
- 
+import {Header, Footer} from './components'; 
+import { Allroutes } from './routes/Allroutes';
  
 
 export const App = () => {
   return (
     <div>
- 
+      <Header />
+        <Allroutes />
+      <Footer />
     </div>
   );
 }
