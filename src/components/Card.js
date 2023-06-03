4c60be194d4218b6a100d66ddd581a3c4af808a6
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-03 23:07:14 +0530

    add card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,21 @@
- 
+import {Link} from 'react-router-dom';
+import Backup from '../assets/backup.jpg';
 
-export const Card = () => {
+export const Card = ({movie}) => {
+   const {poster_path, original_title, overview, id } = movie;
+   const pic = poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : Backup ;
+
   return (
-    <div>Card</div>
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
+              <Link to={`movie/${id}`}>
+                  <img src={pic} alt={original_title} />
+              </Link>
+        <div className="p-5">
+           <Link to={`movie/${id}`}>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
+           </Link>
+           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+          </div>
+       </div>
   )
 }
