29bb4e7c7cbe17719037c84d4444fc2e769881ee
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-03 23:08:36 +0530

    custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,8 @@
 import { useEffect, useState} from "react";
-export const useFetch = (url) => {
+
+export const useFetch = (apiPath, queryName="") => {
     const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryName}`
     useEffect(()=>{
      const fetchData = async()=>{
         const response = await fetch(url);
