c7e092e4d7d5a6505ac89c1983469ba8ce166f8d
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-03 23:07:56 +0530

    make search functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
- import {Link, NavLink} from 'react-router-dom';
+ import {Link, NavLink, useNavigate} from 'react-router-dom';
  import Logo from '../assets/logo.svg';
 import { useEffect, useState } from 'react';
 import Chewbacca from '../assets/chewbacca.svg';
@@ -12,11 +12,20 @@ export const Header = () => {
   const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) || false);
   const activeClass = 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500';
   const inActiveClass = 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700';
+  const navigate = useNavigate();
+
   
   useEffect(()=>{
     localStorage.setItem('darkMode',JSON.stringify(darkMode));
     darkMode ?   document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
   },[darkMode])
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const queryName = e.target.search.value; 
+    e.target.reset();
+    return navigate(`/search?q=${queryName}`)
+  }
 
   return (
     <header >
@@ -50,10 +59,16 @@ export const Header = () => {
                 <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                 <span className="sr-only" >Search icon</span>
               </div>
-              <input
+           
+            <form onSubmit={handleSubmit}>
+            <input
+                name='search'
                 type="text"
                 id="search-navbar"
                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Movies..." />
+            </form>
+
+
             </div>
 
             {/* toggle search */}
@@ -78,10 +93,16 @@ export const Header = () => {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                   <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                 </div>
+               
+                <form  onSubmit={handleSubmit}>
                 <input
+                name='search'
                 type="text"
                 id="search-navbar"
                 className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Movies..." />
+                </form>
+              
+              
               </div>
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
