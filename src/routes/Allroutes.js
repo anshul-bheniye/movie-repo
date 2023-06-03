e5a0a6f03af5f69119b8bf2b326415c6a0ba14df
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-03 13:14:02 +0530

    give dark mode bg

--- src/routes/Allroutes.js
+++ src/routes/Allroutes.js
@@ -3,14 +3,16 @@ import {MovieList, MovieDetail, Search, PageNotFound} from '../pages';
 
 export const Allroutes = () => {
   return (
-    <Routes>
-        <Route path='' element={<MovieList />}></Route>
-        <Route path='movie/:id' element={<MovieDetail />}></Route>
-        <Route path='popular' element={<MovieList />}></Route>
-        <Route path='topRated' element={<MovieList />}></Route>
-        <Route path='search' element={<Search />}></Route>
-        <Route path='upcoming' element={<MovieList />}></Route>
-        <Route path='*' element={<PageNotFound />}></Route>
-    </Routes>
+    <main className='dark:bg-slate-800 min-h-screen'>
+      <Routes>
+          <Route path='' element={<MovieList />}></Route>
+          <Route path='movie/:id' element={<MovieDetail />}></Route>
+          <Route path='popular' element={<MovieList />}></Route>
+          <Route path='topRated' element={<MovieList />}></Route>
+          <Route path='search' element={<Search />}></Route>
+          <Route path='upcoming' element={<MovieList />}></Route>
+          <Route path='*' element={<PageNotFound />}></Route>
+      </Routes>
+    </main>
   )
 }
