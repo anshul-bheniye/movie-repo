ff0776dd92e63704910bc2a6308bce804dc890f9
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-04 22:42:17 +0530

    refactor

--- src/routes/Allroutes.js
+++ src/routes/Allroutes.js
@@ -5,15 +5,14 @@ export const Allroutes = () => {
   return (
     <main className='dark:bg-slate-800 min-h-screen'>
       <Routes>
-          <Route path='' element={<MovieList apiPath={'movie/now_playing'} />}></Route>
+          <Route path='' element={<MovieList apiPath='movie/now_playing' />}></Route>
           <Route path='movie/:id' element={<MovieDetail />}></Route>
-          <Route path='popular' element={<MovieList apiPath={'movie/popular'} />}></Route>
-          <Route path='topRated' element={<MovieList apiPath={'movie/top_rated'} />}></Route>
+          <Route path='popular' element={<MovieList apiPath='movie/popular' />}></Route>
+          <Route path='topRated' element={<MovieList apiPath='movie/top_rated' />}></Route>
           <Route path='search' element={<Search apiPath='search/movie' />}></Route>
-          <Route path='upcoming' element={<MovieList apiPath={'movie/upcoming'} />}></Route>
+          <Route path='upcoming' element={<MovieList apiPath='movie/upcoming' />}></Route>
           <Route path='*' element={<PageNotFound />}></Route>
       </Routes>
     </main>
   )
 }
-// movie/{movie_id} for movieDetail
