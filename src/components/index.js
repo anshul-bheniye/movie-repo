c197af0b1913e031c23e4e96b33c56a73898b7c9
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-04 22:43:31 +0530

    index card

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export {Header} from '../components/Header';
 
 export {Card} from '../components/Card';
+export {MovieDetailCard} from '../components/MovieDetailCard';
 
 export {Footer} from '../components/Footer';
