b046be3c3abb8c6fd56f9402606d1407605234d2
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-04 20:39:06 +0530

    match bg with header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Footer = () => {
   return (
   
-<footer className="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+<footer className="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 dark:border-gray-600">
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
         <a href="https://www.linkedin.com/in/anshul-bheniye/" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
