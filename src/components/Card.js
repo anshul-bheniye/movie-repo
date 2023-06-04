a3457d4e93ed0cfdc9c2a770b7fad2cbcee0bfa3
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-04 22:46:25 +0530

    fix forward slash that caused 404 on every route

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,11 +7,11 @@ export const Card = ({movie}) => {
 
   return (
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
-              <Link to={`movie/${id}`}>
+              <Link to={`/movie/${id}`}>
                   <img src={pic} alt={original_title} />
               </Link>
         <div className="p-5">
-           <Link to={`movie/${id}`}>
+           <Link to={`/movie/${id}`}>
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
            </Link>
            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
