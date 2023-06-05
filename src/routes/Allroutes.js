087acff7e711fa91a07f1ff27c05c1d5105b24d3
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-06-05 12:09:31 +0530

    pass title prop to element

--- src/routes/Allroutes.js
+++ src/routes/Allroutes.js
@@ -5,13 +5,13 @@ export const Allroutes = () => {
   return (
     <main className='dark:bg-slate-800 min-h-screen'>
       <Routes>
-          <Route path='' element={<MovieList apiPath='movie/now_playing' />}></Route>
+          <Route path='' element={<MovieList apiPath='movie/now_playing'  title='Home'/>}></Route>
           <Route path='movie/:id' element={<MovieDetail />}></Route>
-          <Route path='popular' element={<MovieList apiPath='movie/popular' />}></Route>
-          <Route path='topRated' element={<MovieList apiPath='movie/top_rated' />}></Route>
+          <Route path='popular' element={<MovieList apiPath='movie/popular' title='Popular'/>}></Route>
+          <Route path='topRated' element={<MovieList apiPath='movie/top_rated' title='Top Rated'/>}></Route>
           <Route path='search' element={<Search apiPath='search/movie' />}></Route>
-          <Route path='upcoming' element={<MovieList apiPath='movie/upcoming' />}></Route>
-          <Route path='*' element={<PageNotFound />}></Route>
+          <Route path='upcoming' element={<MovieList apiPath='movie/upcoming' title='Upcoming'/>}></Route>
+          <Route path='*' element={<PageNotFound title='404 Page Not Found' />}></Route>
       </Routes>
     </main>
   )
